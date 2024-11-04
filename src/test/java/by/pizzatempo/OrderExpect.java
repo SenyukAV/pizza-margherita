0d283a5d8a4e54cc490e0fed43375009c4bbddef
Aliaksandr Sianiuk <avsenyuk@gmail.com>
2024-11-04 23:37:45 +0300

    add OrderExpect

---
+++ src/test/java/by/pizzatempo/OrderExpect.java
@@ -0,0 +1,8 @@
+package by.pizzatempo;
+
+import java.util.HashSet;
+
+public class OrderExpect {
+    public final static String EXPECT_DRINK_TEXT = "Квас Аливарский Dark Side,1 литр";
+    public final static String EXPECT_PIZZA_TEXT = "Пицца \"Маргарита\" (21см)";
+}
